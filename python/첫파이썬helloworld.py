6d7e893247f1e49d4d3eea580832847d74447277
user.email <rorok123@naver.com>
2019-12-19 11:18:56 +0900

    helloworld 3줄 동시출력

---
+++ python/첫파이썬helloworld.py
@@ -0,0 +1,11 @@
+Python 3.7.5 (tags/v3.7.5:5c02a39a0b, Oct 15 2019, 00:11:34) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a = "Hello world"
+>>> for i in [a,a,a]:
+	print(a)
+
+	
+Hello world
+Hello world
+Hello world
+>>> 
